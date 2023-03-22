ffbe24f63acdc78bfb5cce2a55049c748a0b50b6
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-03-22 23:55:37 +0100

    views

--- ProjectAY/apps/auths/views.py
+++ ProjectAY/apps/auths/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from helpers.views import BaseView
+from .models import Primary
+from rest_framework.response import Response
 
 # Create your views here.
+class CreateUser(BaseView):
+    def post(self, request, format=None):
+        res = super().post(request, format)
+        if res:
+            return res
+        
+        email = request.data["email"]
+        user = Primary.objects.filter(email=email).first()
+        if user:
+            resp = {
+                "code": 400,
+                "message": "We regret to inform you that an account with the email address provided already exists in our system. Please try logging in with your email and password."
+            }
+            
+        
+        
+        
