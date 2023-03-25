1a249d1c0525b3ecf73b829ce2378dff9975124f
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-03-25 22:48:01 +0100

    Deployment ready

--- ProjectAY/config/settings.py
+++ ProjectAY/config/settings.py
@@ -36,6 +36,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 ALLOWED_HOSTS = ["*"]
 
 
@@ -62,6 +64,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
